acbd73560117c86805b5c0149a462fa68df8b283
Suroj Lamichhane <lamichhane.suroj@gmail.com>
2020-02-23 23:10:11 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,385 @@
+﻿namespace AS5000_Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.button5 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            this.tabControl2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 285);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sign Up";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(137, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(176, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(137, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(176, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(137, 105);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(176, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(65, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Title";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(65, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Userlevel";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(68, 325);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Deactivate User";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(68, 364);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Activate User";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(137, 144);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(137, 182);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(68, 219);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Is Active ?";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(137, 219);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 16;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(2, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(761, 540);
+            this.tabControl1.TabIndex = 17;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.textBox4);
+            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.checkBox1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.comboBox2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(753, 514);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Sign Up";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(68, 404);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 23);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Delete User";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(319, 287);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(140, 20);
+            this.textBox4.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(204, 290);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(109, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Generated Password:";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(753, 514);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Details";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 10);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(720, 493);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.button5);
+            this.tabPage3.Controls.Add(this.textBox5);
+            this.tabPage3.Controls.Add(this.label8);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(253, 116);
+            this.tabPage3.TabIndex = 0;
+            this.tabPage3.Text = "Authenticate";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(84, 45);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(107, 20);
+            this.textBox5.TabIndex = 1;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(25, 48);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Password";
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Controls.Add(this.tabPage3);
+            this.tabControl2.Location = new System.Drawing.Point(286, 169);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(261, 142);
+            this.tabControl2.TabIndex = 20;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(100, 71);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Login";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 543);
+            this.Controls.Add(this.tabControl2);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "AS5000 User management";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabControl2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AS5000_Login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();            
+
+            dataGridView1.BackgroundColor = Color.White;
+
+            authenticate();
+            loaddropdowns();
+            loadalluser();
+        }
+
+        private void authenticate()
+        {
+
+            tabControl1.Visible = false;
+            tabControl2.Visible = true;
+
+
+
+
+        }
+
+        private void loaddropdowns()
+        {
+            comboBox1.Items.Add("Admin");
+            comboBox1.Items.Add("CM");
+            comboBox1.Items.Add("DM");
+            comboBox1.Items.Add("ICC");
+            comboBox1.Items.Add("NSD");
+            comboBox1.Items.Add("PD");
+            comboBox1.Items.Add("RM");
+            comboBox1.Items.Add("TM");
+            comboBox1.Items.Add("Senior Scientist");
+            comboBox1.Items.Add("Analytics / Marketing");
+
+            comboBox2.Items.Add("Admin");
+            comboBox2.Items.Add("User");
+        }
+
+
+        private void loadalluser()
+        {
+            List<string> parameters = new List<string>();
+            List<string> values = new List<string>();
+
+            //parameters.Add("@id");
+            //values.Add("550");
+
+            DataSet ds = Dataaccess("SelectAllUser", 1, parameters, values);
+
+            dataGridView1.DataSource = ds.Tables[0];
+            dataGridView1.Refresh();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string firstname;
+            string lastname;
+            string email;
+
+            string title;
+            string userlevel;
+            string usertype;
+
+            string password;
+
+            List<string> parameter = new List<string>();
+            List<string> value = new List<string>();
+
+            DataSet ds = new DataSet();
+
+            firstname = textBox1.Text;
+            lastname = textBox2.Text;
+            email = textBox3.Text;
+
+            if (comboBox1.SelectedIndex < 0)
+            {
+                MessageBox.Show("Missing input fields!");
+                return;
+            }
+            if (comboBox2.SelectedIndex < 0)
+            {
+                MessageBox.Show("Missing input fields!");
+                return;
+            }
+
+            title = comboBox1.SelectedItem.ToString();
+            userlevel = comboBox2.SelectedItem.ToString();
+
+            if (firstname == "" && lastname == "" && email == "" && title == "" && userlevel == "")
+            {
+                MessageBox.Show("Missing input fields!");
+                return;
+            }
+
+            if(!checkBox1.Checked)
+            {
+                email = email + "-old";
+            }
+
+            if(userlevel=="Admin")
+            {
+                usertype = "2";
+            }
+            else
+            {
+                usertype = "1";
+            }
+
+            parameter.Add("@firstname");
+            parameter.Add("@lastname");
+            parameter.Add("@email");
+            parameter.Add("@title");
+            parameter.Add("@userlevel");
+
+            value.Add(firstname);
+            value.Add(lastname);
+            value.Add(email);
+            value.Add(title);
+            value.Add(usertype);
+
+            try
+            {
+                ds = Dataaccess("AddNewUser", 1, parameter, value);
+                password = ds.Tables[0].Rows[0]["Password"].ToString();
+                textBox4.Text = password;
+
+                MessageBox.Show("Successfully added user: " + firstname + " " + lastname);
+            }
+            catch
+            {
+                MessageBox.Show("Failed adding new user. Try again");
+            }
+
+            loadalluser();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            List<string> parameter = new List<string>();
+            List<string> value = new List<string>();
+
+            if (textBox3.Text == "")
+            {
+                MessageBox.Show("Email is required!");
+                return;
+            }
+
+
+            parameter.Add("@email");
+            parameter.Add("@type");
+
+            value.Add(textBox3.Text);
+            value.Add("0");
+
+            try
+            {
+                Dataaccess("ChangeUserDetail", 1, parameter, value);
+                MessageBox.Show("Successfully deactivated user: " + textBox3.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Failed. Try again!");
+            }
+
+            loadalluser();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            List<string> parameter = new List<string>();
+            List<string> value = new List<string>();
+
+            if (textBox3.Text == "")
+            {
+                MessageBox.Show("Email is required!");
+                return;
+            }
+
+            parameter.Add("@email");
+            parameter.Add("@type");
+
+            value.Add(textBox3.Text);
+            value.Add("1");
+
+            try
+            {
+                Dataaccess("ChangeUserDetail", 1, parameter, value);
+                MessageBox.Show("Successfully activated user: " + textBox3.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Failed. Try again!");
+            }
+           
+
+            loadalluser();
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            List<string> parameter = new List<string>();
+            List<string> value = new List<string>();
+
+            if (textBox3.Text == "")
+            {
+                MessageBox.Show("Email is required!");
+                return;
+            }
+
+            parameter.Add("@email");
+            parameter.Add("@type");
+
+            value.Add(textBox3.Text);
+            value.Add("2");
+
+            try
+            {
+                Dataaccess("ChangeUserDetail", 1, parameter, value);
+                MessageBox.Show("Successfully deleted user: " + textBox3.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Failed. Try again!");
+            }
+            
+
+            loadalluser();
+        }
+
+
+        private DataSet Dataaccess(string fullcommand, int type, List<string> parameters, List<string> values)
+        {
+            string ConnectionString = null;
+            string sql = null;
+
+            SqlConnection connection;
+            SqlCommand command;
+            SqlDataAdapter adapter;
+
+            DataSet ds = new DataSet(); 
+            
+            ConnectionString = ConfigurationManager.AppSettings["ConnectionString"];
+            //ConnectionString = "Data Source = arcticsun5000.ccnsypazwmb3.us - east - 2.rds.amazonaws.com; Initial Catalog = Bard; User ID = Administrator; Password = t!JCwU = (9.nxb - tHn)5XOqgFGR.KABg2";
+
+            sql = fullcommand;
+            connection = new SqlConnection(ConnectionString);
+
+            int i = 0;
+
+            try
+            {
+                connection.Open();
+                command = new SqlCommand(sql, connection);
+
+                if (type == 1)
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    
+
+                    foreach(string s in parameters)
+                    {
+                        command.Parameters.AddWithValue(s, values[i]);
+
+                        i++;
+                    }
+
+                    
+                }
+                else
+                {
+
+                }
+                
+
+                
+                adapter = new SqlDataAdapter(command);
+
+                adapter.Fill(ds);
+
+                adapter.Dispose();               
+                command.Dispose();
+                connection.Close();
+
+               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return ds;
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            if (textBox5.Text == "arcticcircle5000!")
+            {
+                tabControl1.Visible = true;
+                tabControl2.Visible = false;
+                loadalluser();
+            }
+            else
+            {
+                tabControl1.Visible = false;
+                tabControl2.Visible = true;
+                MessageBox.Show("Wrong Password. Try again!");
+            }
+        }
+    }
+}
